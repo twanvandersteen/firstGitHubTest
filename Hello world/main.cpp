20234ceafd77d0621d1725ac06db648428a6db04
Twan van der Steen <Twan.van.der.Steen@sioux.eu>
2016-07-30 17:28:41 +0200

    Feature added

--- Hello world/main.cpp
+++ Hello world/main.cpp
@@ -2,8 +2,19 @@
 
 using namespace std;
 
+
+void forloopHello(void)
+{
+	for (int counter = 0; counter < 10 ; counter++)
+    {
+        cout << "Hello THJ\r\n";
+
+    }
+}
+
 int main()
 {
     cout << "Hello world!!!" << endl;
+    forloopHello();
     return 0;
 }
